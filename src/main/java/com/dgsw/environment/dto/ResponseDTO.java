e0f99c5fefeeb9ea0b524f261d243cd92459795b
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-06-22 00:04:15 +0900

    feat: add ResponseDTO

---
+++ src/main/java/com/dgsw/environment/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.dgsw.environment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ResponseDTO {
+    private String message;
+}
