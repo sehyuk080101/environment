661f5063c15133a3f74b6334fc1379efe3018788
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-06-22 00:04:28 +0900

    feat: add GlobalExceptionHandler

---
+++ src/main/java/com/dgsw/environment/global/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.dgsw.environment.global;
+
+import com.dgsw.environment.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseDTO handleException(RuntimeException e) {
+        log.error(e.getMessage(), e);
+
+        return new ResponseDTO(e.getMessage());
+    }
+}
