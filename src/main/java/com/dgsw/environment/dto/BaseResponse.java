e0f187177a77399582401c9317d533a47f764a21
tpqls774 <tpqls774@dgsw.hs.kr>
2025-06-25 17:09:38 +0900

    change

--- src/main/java/com/dgsw/environment/dto/BaseResponse.java
+++ src/main/java/com/dgsw/environment/dto/BaseResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @Setter
@@ -15,5 +17,29 @@ public class BaseResponse<T> {
 
     public BaseResponse(String message) {
         this.message = message;
+    }
+
+    public static <T> ResponseEntity<BaseResponse<T>> of(T data, String message, HttpStatus status) {
+        return ResponseEntity.status(status).body(new BaseResponse<T>(data, message));
+    }
+
+    public static <T> ResponseEntity<BaseResponse<T>> ok(T data, String message) {
+        return of(data, message, HttpStatus.OK);
+    }
+
+    public static <T> ResponseEntity<BaseResponse<T>> created(T data, String message) {
+        return of(data, message, HttpStatus.CREATED);
+    }
+
+    public static ResponseEntity<BaseResponse<Void>> of(String message, HttpStatus status) {
+        return ResponseEntity.status(status).body(new BaseResponse<>(message));
+    }
+
+    public static ResponseEntity<BaseResponse<Void>> ok(String message) {
+        return of(message, HttpStatus.OK);
+    }
+
+    public static ResponseEntity<BaseResponse<Void>> created(String message) {
+        return of(message, HttpStatus.CREATED);
     }
 }
